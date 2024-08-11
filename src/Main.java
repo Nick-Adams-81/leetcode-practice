833f3507f140d70bf6f8a4d4eef472a4181e1fc0
Nick Adams <nick.pappas.adams@gmail.com>
2024-08-11 12:22:07 -0700

    Work in progress

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import ArraysAndHashing.ContainsDuplicate;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+
+    public static void main(String[] args) {
+        ContainsDuplicate duplicate = new ContainsDuplicate();
+        int[] dups = {1,2,3,4};
+        System.out.println(duplicate.duplicate(dups));
+
+
+        }
+
+}
