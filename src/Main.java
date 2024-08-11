d39344f17b4e2043046c63e54012f4f3370ec969
Nick Adams <nick.pappas.adams@gmail.com>
2024-08-11 12:25:27 -0700

    Work in progress

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import ArraysAndHashing.ContainsDuplicate;
+import ArraysAndHashing.IsAnagram;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -6,8 +7,12 @@ public class Main {
 
     public static void main(String[] args) {
         ContainsDuplicate duplicate = new ContainsDuplicate();
+        IsAnagram isAnagram = new IsAnagram();
         int[] dups = {1,2,3,4};
+        String s1 = "anagram";
+        String s2 = "nagarmm";
         System.out.println(duplicate.duplicate(dups));
+        System.out.println(isAnagram.isAnagram(s1, s2));
 
 
         }
