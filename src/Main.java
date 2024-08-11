64a71178102f0bc6ab85cb3375e13c6c40921b8d
Nick Adams <nick.pappas.adams@gmail.com>
2024-08-11 14:10:39 -0700

    Work in progress

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import ArraysAndHashing.ContainsDuplicate;
+import ArraysAndHashing.GroupAnagrams;
 import ArraysAndHashing.IsAnagram;
 import ArraysAndHashing.TwoSum;
 
@@ -12,13 +13,16 @@ public class Main {
         ContainsDuplicate duplicate = new ContainsDuplicate();
         IsAnagram isAnagram = new IsAnagram();
         TwoSum twoSums = new TwoSum();
+        GroupAnagrams groupAnagrams = new GroupAnagrams();
         int[] dups = {1,2,3,4};
         int[] nums = {3,4,5,6};
         String s1 = "anagram";
         String s2 = "nagarmm";
+        String[] groups = {"hello", "olleh", "hi", "ih", "lolhe", "car", "rac", "arc"};
         System.out.println(duplicate.duplicate(dups));
         System.out.println(isAnagram.isAnagram(s1, s2));
         System.out.println(Arrays.toString(twoSums.twoSum(nums, 7)));
+        System.out.println(groupAnagrams.groupAnagrams(groups));
 
 
         }
