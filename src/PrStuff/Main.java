50d9f5ef6347ff8bc965947f1629c34df58aa418
Nick Adams <nick.pappas.adams@gmail.com>
2024-09-28 13:18:14 -0700

    Work in progress

--- src/PrStuff/Main.java
+++ src/PrStuff/Main.java
@@ -1,8 +1,5 @@
 package PrStuff;
-import PrStuff.TwoPointer.ReverseString;
-import PrStuff.TwoPointer.SortByParity;
-import PrStuff.TwoPointer.TwoSum;
-import PrStuff.TwoPointer.ValidPalindrome;
+import PrStuff.TwoPointer.*;
 
 import java.util.Arrays;
 
@@ -13,14 +10,18 @@ public class Main {
         ValidPalindrome validPalindrome = new ValidPalindrome();
         SortByParity sortByParity = new SortByParity();
         TwoSum twoSum = new TwoSum();
+        MergeTwoSortedArrays mergeTwoSortedArrays = new MergeTwoSortedArrays();
 
         int[] nums = {5,1,2,4,3,6,9,8,7};
         int[] nums2 = {1, 3, 6 ,8};
+        int[] nums3 = {2, 4, 6 ,8};
+        int[] nums4 = {1, 3, 5, 7};
         sortByParity.printArray(nums);
         sortByParity.sort(nums);
         sortByParity.printArray(nums);
 
-
+        int[] result = mergeTwoSortedArrays.merge(nums3, nums4);
+        System.out.println(Arrays.toString(result));
         System.out.println(Arrays.toString(twoSum.twoSum(nums2, 11)));
         System.out.println(validPalindrome.valid("A man, a plan, a canal: Panama!"));
         System.out.println(reverseString.reverse("hello world"));
